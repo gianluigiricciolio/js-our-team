bdd328753af2132b604ffaff983b66948378676d
gianluigiricciolio <gianluigiricciolio@proton.me>
2024-05-14 16:15:42 +0200

    [UPD] milestone 0 and 1

---
+++ main.js
@@ -0,0 +1,50 @@
+'use strict';
+
+const teamList = [
+    {
+        nome: "Wayne Barnett",
+        ruolo: "Founder & CEO",
+        imgSrc: "wayne-barnett-founder-ceo.jpg"
+    },
+
+    {
+        nome: "Angela Caroll",
+        ruolo: "Chief Editor",
+        imgSrc: "wayne-barnett-founder-ceo.jpg"
+    },
+
+    {
+        nome: "Wayne Barnett",
+        ruolo: "Founder & CEO",
+        imgSrc: "wayne-barnett-founder-ceo.jpg"
+    },
+
+    {
+        nome: "Wayne Barnett",
+        ruolo: "Founder & CEO",
+        imgSrc: "wayne-barnett-founder-ceo.jpg"
+    },
+
+    {
+        nome: "Wayne Barnett",
+        ruolo: "Founder & CEO",
+        imgSrc: "wayne-barnett-founder-ceo.jpg"
+    },
+
+    {
+        nome: "Wayne Barnett",
+        ruolo: "Founder & CEO",
+        imgSrc: "wayne-barnett-founder-ceo.jpg"
+    }
+]
+
+for (let i=0; i < teamList.length; i++){
+    console.log (teamList[i]);
+}
+
+// function imgSrcGenerator () {
+//     let source = "";
+
+
+//     return source;
+// }
